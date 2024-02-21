dc8dd75ade880bcb541bb66f24e9d53f486549fa
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-20 21:02:30 -0600

    simulacion de login para probar los endpoints privados creado

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -12,7 +12,36 @@ const user ={
     password: 'enrique@gmail.com',
     phone: '1234567890'
 }
+//simular primero el login para los endpoints privados
+beforeAll(async() => {
+    const user = {
+        email: 'luis@gmail.com',
+        password: 'luis12345'
+    };
 
+    const res = await request(app)
+        .post(`${URL_USERS}/login`)
+        .send(user)
+
+    TOKEN = res.body.token;
+    
+})
+
+
+
+//-------Test GET ALL-----------
+test("Get -> URL_USERS, should return statusCode 200, res.body to be defined and res.body.length = 1", async() => {
+    const res = await request(app)
+        .get(URL_USERS)
+        .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toHaveLength(1);
+});
+
+
+//-------Test POST-CREATE-----------
 test("Post 'URL_USERS', should return statusCode 201,and res.body to be defined and res.body.firstName = user.firstName", async() => {
     const res = await request(app)
         .post(URL_USERS)
@@ -22,4 +51,5 @@ test("Post 'URL_USERS', should return statusCode 201,and res.body to be defined 
     expect(res.statusCode).toBe(201);
     expect(res.body).toBeDefined();
     expect(res.body.firstName).toBe(user.firstName);
-})
+});
+
