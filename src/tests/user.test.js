860bb6cec9629deb6de2ddc0f8db9740e5d84571
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-20 22:09:06 -0600

    Tests agregados y funcionales

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,13 +4,18 @@ const User = require("../models/User");
 
 const URL_USERS = "/users";
 let userId;
+let TOKEN;
 
 const user ={
     firstName: 'Enrique',
     lastName: 'Gonzalez',
     email: 'enrique@gmail.com',
-    password: 'enrique@gmail.com',
+    password: 'enrique12345',
     phone: '1234567890'
+}
+
+const newNameUser = {
+    firstName: 'newName'
 }
 //simular primero el login para los endpoints privados
 beforeAll(async() => {
@@ -53,3 +58,55 @@ test("Post 'URL_USERS', should return statusCode 201,and res.body to be defined 
     expect(res.body.firstName).toBe(user.firstName);
 });
 
+
+//-------Test PUT--------------
+test("Put -> 'URL_USERS/:id', should return statusCode 200, res.body to be defined and res.body.firstName = newNameUser.firstName", async() => {
+    const res = await request(app)
+        .put(`${URL_USERS}/${userId}`)
+        .set('Authorization', `Bearer ${TOKEN}`)
+        .send(newNameUser)
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.firstName).toBe(newNameUser.firstName);
+});
+
+//-----------TEST LOGIN---------------
+test("Post -> 'URL_USERS/login', should return status 200, res.body to be defined,  res.body.email === user.email and res.body.token to be defined", async() => {
+
+    const userLogin = {
+        email: 'enrique@gmail.com',
+        password: 'enrique12345'
+    }
+
+    const res = await request(app)
+        .post(`${URL_USERS}/login`)
+        .send(userLogin)
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.user.email).toBe(userLogin.email);
+    expect(res.body.token).toBeDefined();
+});
+
+//-----------TEST FAIL LOGIN----------------
+test("Post -> 'URL_USERS/login', should return status 401", async() => {
+    const failUserLogin = {
+        email: 'enrique@gmail.com',
+        password: 'failpassword'  //fail password
+    }
+    const res = await request(app)
+        .post(`${URL_USERS}/login`)
+        .send(failUserLogin) 
+
+    expect(res.statusCode).toBe(401);
+})
+
+//-------Test DELETE--------------\
+test("Delete -> 'URL_USERS', should return statusCode 204", async() => {
+    const res = await request(app)
+        .delete(`${URL_USERS}/${userId}`)
+        .set('Authorization', `Bearer ${TOKEN}`)
+    
+    expect(res.statusCode).toBe(204);
+});
