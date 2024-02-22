e40f76fccdb465a1f61d3bd1558467912b369be3
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-21 21:40:10 -0600

    cree la relacion entre products y categories

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,6 @@
 const User = require("./User");
 const Category = require("./Category");
+const Product = require("./Product");
+
+Product.belongsTo(Category); // se agrega el campo categoryId
+Category.hasMany(Product);
