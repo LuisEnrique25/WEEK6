b3d55721dc251e886175bc5f243dc24fab595956
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-23 02:10:25 -0600

    CRUD de cart completado

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,16 @@
 const User = require("./User");
 const Category = require("./Category");
 const Product = require("./Product");
+const Cart = require("./Cart");
 
 Product.belongsTo(Category); // se agrega el campo categoryId
 Category.hasMany(Product);
+
+
+//Cart + User => userId en tabla products
+Cart.belongsTo(User);
+User.hasMany(Cart);
+
+//Cart + Product => productId en tabla products
+Cart.belongsTo(Product);
+Product.hasMany(Cart);
