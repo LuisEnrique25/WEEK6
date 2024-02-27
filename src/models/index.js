438e4e56a729aceebd8839107bc582873dcec1cc
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-27 17:18:25 -0600

    cart y purchase agregados y funcionales

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ const User = require("./User");
 const Category = require("./Category");
 const Product = require("./Product");
 const Cart = require("./Cart");
+const Purchase = require("./Purchase");
 
 Product.belongsTo(Category); // se agrega el campo categoryId
 Category.hasMany(Product);
@@ -14,3 +15,12 @@ User.hasMany(Cart);
 //Cart + Product => productId en tabla products
 Cart.belongsTo(Product);
 Product.hasMany(Cart);
+
+//Purchase + User => userId en tabla purchase
+Purchase.belongsTo(User);
+User.hasMany(Purchase);
+
+//Purchase + Product => productId en tabla purchase
+Purchase.belongsTo(Product);
+Product.hasMany(Purchase);
+
